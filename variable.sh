177d13968f0ba43408786f663fbc0a7db9a81939
surekha25 <surekhasuri25@gmail.com>
2025-09-22 12:49:49 +0530

    new file added

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$person1=Ramesh
+$person2=Suresh
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: What you are leaning now?"
+echo "$person2:: I am learning shell scription, $person1"
