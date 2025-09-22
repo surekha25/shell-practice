efa0bfc9b2e758867db97b0364a410dcd664fb77
surekha25 <surekhasuri25@gmail.com>
2025-09-22 14:21:33 +0530

    Argument Variables Example

---
+++ arugument_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: What you are leaning now?"
+echo "$person2:: I am learning shell scription, $person1"
