ffb26795b01bedf232d96afb886b7f356f316f3d
surekha25 <surekhasuri25@gmail.com>
2025-09-29 10:54:13 +0530

    conditions in shell script

--- 08.data_types.sh
+++ 08.data_types.sh
@@ -10,6 +10,12 @@ echo "sum is ${sum}"
 
 #array
 
+# size=4 max index=3
 leaders=("modi" "putin" "trudo" "trump")
 
 echo "All leaders: ${leaders[@]}"
+
+echo "First Leader: ${leaders[0]}"
+echo "Second Leader: ${leaders[1]}"
+echo "Third Leader: ${leaders[2]}"
+echo "Fourth Leader: ${leaders[3]}"
---
+++ 09.conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number=$1;
+
+echo "Please Enter Number :"
+read $number
+
+if[$number -gt 10]; then
+    echo "The Number $number is greather than 10
+else 
+    echo "The Number $number is less than 10
+fi
