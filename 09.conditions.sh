82e078c11e7e1ede090a125c4e2f77c4b2919cfe
surekha25 <surekhasuri25@gmail.com>
2025-09-29 11:08:44 +0530

    conditions in shell script

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-number=$1;
+number=$1
 
-echo "Please Enter Number :"
-read $number
-
-if[ $number -gt 10 ] then
+if [ $number -gt 10 ]; then
     echo "The Number $number is greather than 10"
 else 
     echo "The Number $number is less than 10"
