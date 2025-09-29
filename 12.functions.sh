a44248d4936b12c9d21e52a26e99e607eb21577c
surekha25 <surekhasuri25@gmail.com>
2025-09-29 12:34:54 +0530

    Functions - Installing Mysql, Nginx, Python 3 using shell script

---
+++ 12.functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#by using function we can reduce repeated task lines. please compare 11.installingMysql.sh and 12.functions.sh
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Please run this script with root privilege"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0]; then
+        echo "Error: Installing $2 is Failed"
+        exit 1
+    else 
+        echo "Success: Installing $2 is Success"
+}
+
+dnf install mysql -q
+validate $? "MySQL"  #passing args 'exit status' and 'MySQL'
+
+dnf install nginx -q
+validate $? "Nginx"
+
+dnf install python3 -q
+validate $? "Python 3"
+
