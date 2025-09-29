d997002c6ffbd075ce6b346e0b30b1c1fba324a7
surekha25 <surekhasuri25@gmail.com>
2025-09-29 12:59:18 +0530

    Colors using shell script

---
+++ 13.colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+black="\e[30m"
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+blue="\e[34m"
+purple="\e[35m"
+cyan="\e[36m"
+white="\e[37m" #"\e[0m"
+
+echo -e "$cyan Hello World"
+echo "This is Hyderabad $N"
+echo "check this line of color"
