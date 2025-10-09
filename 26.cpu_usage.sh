9f2f2c1add7ffb5f10ff8c0d2572c1ee76e98846
surekha25 <surekhasuri25@gmail.com>
2025-10-09 11:58:37 +0530

    CPU Usage File ADDED

---
+++ 26.cpu_usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cpu_threshold=80
+ip_address=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+message=""
+
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+cpu_usage=${cpu_usage%.*}  # remove decimal
+if [ $cpu_usage -ge $cpu_threshold ]; then
+    message+="<b>High CPU Usage</b>: $cpu_usage% <br>"
+fi
+
+echo -e "Message Body: $message"
+
+sh mail.sh "info@arunodhai.com" "High CPU Usage Alert" "High CPU Usage" "$message" "$ip_address" "Devops Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
+# TO_TEAM=$6
