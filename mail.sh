fffd555fb4a531c9a02d57a8ab1dba0adf0383ba
surekha25 <surekhasuri25@gmail.com>
2025-10-09 11:39:48 +0530

    disk usage update code

--- mail.sh
+++ mail.sh
@@ -4,7 +4,7 @@ TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 MESSAGE_BODY=$4
-FORMATTED_BODY=$(printf '%s' "$MESSAGE_BODY" | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")
+FORMATTED_BODY=$(printf '%s\n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
 IP_ADDRESS=$5
 TO_TEAM=$6
 
