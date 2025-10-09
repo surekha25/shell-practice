50c1a68b47a9ae57840427eb0e4cebb76668a202
surekha25 <surekhasuri25@gmail.com>
2025-10-09 11:24:43 +0530

    disk usage update code

--- 25.disk-usage.sh
+++ 25.disk-usage.sh
@@ -2,7 +2,7 @@
 
 disk_usage=$(df -hT | grep -v Filesystem)
 disk_threshold=2 # in project we keep it as 75
-ip_address=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+ip_address=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 message=""
 
 while IFS= read -r line
@@ -10,8 +10,17 @@ do
     usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     partition=$(echo $line | awk '{print $7}')
     if [ $usage -ge $disk_threshold ]; then
-        message+="High Usage on $partition: $usage % \n"
+        message+="High Usage on $partition: $usage% <br>"
     fi
 done <<< $disk_usage
 
 echo -e "Message Body: $message"
+
+sh mail.sh "info@arunodhai.com" "High Disk Usage Alert" "High Disk Usage" "$message" "$ip_address" "Devops Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
+# TO_TEAM=$6
